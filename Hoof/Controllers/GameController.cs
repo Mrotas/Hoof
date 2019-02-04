fc431c6215714c76c6fd6718721bd1b0ae36324d
Mrotas <35173299+Mrotas@users.noreply.github.com>
2019-02-04 19:38:54 +0100

    Move getting game information functionality to dedicated controller

---
+++ Hoof/Controllers/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Domain.Game;
+using Domain.Game.Model;
+
+namespace Hoof.Controllers
+{
+    public class GameController : Controller
+    {
+        private readonly IGameService _gameService;
+
+        public GameController() : this(new GameService())
+        {
+        }
+
+        public GameController(IGameService gameService)
+        {
+            _gameService = gameService;
+        }
+        
+        public JsonResult GetGameModels()
+        {
+            List<GameModel> gameModels = _gameService.GetAllGames();
+
+            return Json(gameModels, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
--- Hoof/Controllers/HuntController.cs
+++ Hoof/Controllers/HuntController.cs
@@ -1,10 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Web.Mvc;
-using Domain.Game;
-using Domain.Game.Model;
-using Domain.GameLoss;
-using Domain.GameLoss.Model;
-using Domain.GameLoss.ViewModel;
 using Domain.Hunt;
 using Domain.Hunt.Models;
 using Domain.Hunt.ViewModels;
@@ -14,18 +9,14 @@ namespace Hoof.Controllers
     public class HuntController : Controller
     {
         private readonly IHuntService _huntService;
-        private readonly IGameService _gameService;
-        private readonly IGameLossService _gameLossService;
 
-        public HuntController() : this(new HuntService(), new GameService(), new GameLossService())
+        public HuntController() : this(new HuntService())
         {
         }
 
-        public HuntController(IHuntService huntService, IGameService gameService, IGameLossService gameLossService)
+        public HuntController(IHuntService huntService)
         {
             _huntService = huntService;
-            _gameService = gameService;
-            _gameLossService = gameLossService;
         }
 
         public ActionResult Index()
@@ -47,13 +38,6 @@ namespace Hoof.Controllers
             int huntsmanId = 1;
             _huntService.Create(model, huntsmanId);
             return Json(new {data = true}, JsonRequestBehavior.AllowGet);
-        }
-
-        public JsonResult GetGameList()
-        {
-            List<GameModel> gameModels = _gameService.GetAllGames();
-
-            return Json(gameModels, JsonRequestBehavior.AllowGet);
         }
     }
 }
